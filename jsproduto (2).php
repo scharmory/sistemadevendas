7d72c848ab8ac165689614285900fa286a8a4541
Aluno <aluno@aluno.com>
2018-11-20 10:10:38 -0200

    teste

--- jsproduto (2).php
+++
@@ -1,7 +0,0 @@
-<?php
-	include_once("model/Produto.class.php");
-	include_once("dao/DaoProduto.class.php");
-	$dao = new DaoProduto ();
-	$produto = $dao->buscarProdutoPorIdNoBanco($_GET['id']);
-	echo $produto->getPrecov();
-?>
--- sair (2).php
+++
@@ -1,9 +0,0 @@
-<?php
-	session_start();
-	session_destroy();
-    unset($_SESSION['usuario']);
-    unset($_SESSION['logado']);
-    
-    header("location: login.php");
-    exit;
-?>
